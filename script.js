29d5dcc5b747b2e03426365829fa19e8feb1f92d
Vinx92 <vv92@tiscali.it>
2024-03-15 20:53:50 +0100

    Modifiche

--- script.js
+++ script.js
@@ -1,140 +1,139 @@
 const DISPLAY = document.getElementById("display");
-const DISPLAY_DUE = document.getElementById('displayDue')
+const DISPLAY_DUE = document.getElementById("displayDue");
 const AC = document.getElementById("ac");
 const CANCELLA = document.getElementById("cancella");
 const UGUALE = document.getElementById("uguale");
 const OPERATORI = document.querySelectorAll(".operatori");
 const NUMERI = document.querySelectorAll(".numeri");
-console.log(DISPLAY);
-console.log(AC);
-console.log(CANCELLA);
-console.log(UGUALE);
-console.log(OPERATORI);
-console.log(NUMERI);
 let operatoreScelto;
 let numeroUno;
 let numeroDue;
 let risultato;
 let risultatoCalcolato = false;
-console.log(operatoreScelto);
 
 for (let i = 0; i < NUMERI.length; i++) {
   NUMERI[i].addEventListener("click", () => {
-    if(risultatoCalcolato === false) {
-    DISPLAY.textContent += NUMERI[i].value;
-    console.log(NUMERI[i])
-  }else{
-    return;
-  }
+    if (DISPLAY_DUE.textContent.includes(".") && NUMERI[i].value === ".") {
+      return;
+    }
+    if (risultatoCalcolato === false) {
+      DISPLAY_DUE.textContent += NUMERI[i].value;
+    } else {
+      return;
+    }
   });
 }
 
 for (let i = 0; i < OPERATORI.length; i++) {
   OPERATORI[i].addEventListener("click", () => {
-    if(operatoreScelto === undefined) {
-    numeroUno = parseFloat(DISPLAY.textContent);
-    operatoreScelto = OPERATORI[i].value;
-    DISPLAY.textContent = "";
-    DISPLAY_DUE.textContent = `${numeroUno} ${operatoreScelto}`;
-    risultatoCalcolato = false;
-  }else {
-    switch (operatoreScelto) {
-      case "+":
-        numeroDue = parseInt(DISPLAY.textContent);
-        risultato = numeroUno + numeroDue;
-        DISPLAY.textContent = risultato;
-        DISPLAY_DUE.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`
-        operatoreScelto = undefined;
-        console.log(operatoreScelto)
-        break;
-  
-      case "-":
-        numeroDue = parseInt(DISPLAY.textContent);
-        risultato = numeroUno - numeroDue;
-        DISPLAY.textContent = risultato;
-        DISPLAY_DUE.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`
-        operatoreScelto = undefined;
-        console.log(operatoreScelto)
-        break;
-  
-      case "/":
-        numeroDue = parseInt(DISPLAY.textContent);
-        risultato = numeroUno / numeroDue;
-        DISPLAY.textContent = risultato;
-        DISPLAY_DUE.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`
-        operatoreScelto = undefined;
-        console.log(operatoreScelto)
-        break;
-  
-      case "*":
-        numeroDue = parseInt(DISPLAY.textContent);
-        risultato = numeroUno * numeroDue;
-        DISPLAY.textContent = risultato;
-        DISPLAY_DUE.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`
-        operatoreScelto = undefined;
-        console.log(operatoreScelto)
-        break;
+    if (operatoreScelto === undefined) {
+      numeroUno = parseFloat(DISPLAY_DUE.textContent);
+      operatoreScelto = OPERATORI[i].value;
+      DISPLAY_DUE.textContent = "";
+      DISPLAY.textContent = `${numeroUno} ${operatoreScelto}`;
+      risultatoCalcolato = false;
+    } else {
+      switch (operatoreScelto) {
+        case "+":
+          risultato = numeroUno + parseFloat(DISPLAY_DUE.textContent);
+          numeroUno = risultato;
+          operatoreScelto = OPERATORI[i].value;
+          DISPLAY_DUE.textContent = "";
+          DISPLAY.textContent = `${numeroUno} ${operatoreScelto}`;
+          break;
+        case "-":
+          risultato = numeroUno - parseFloat(DISPLAY_DUE.textContent);
+          numeroUno = risultato;
+          operatoreScelto = OPERATORI[i].value;
+          DISPLAY_DUE.textContent = "";
+          DISPLAY.textContent = `${numeroUno} ${operatoreScelto}`;
+          break;
+        case "/":
+          risultato = numeroUno / parseFloat(DISPLAY_DUE.textContent);
+          numeroUno = risultato;
+          operatoreScelto = OPERATORI[i].value;
+          DISPLAY_DUE.textContent = "";
+          DISPLAY.textContent = `${numeroUno} ${operatoreScelto}`;
+          break;
+        case "*":
+          risultato = numeroUno * parseFloat(DISPLAY_DUE.textContent);
+          numeroUno = risultato;
+          operatoreScelto = OPERATORI[i].value;
+          DISPLAY_DUE.textContent = "";
+          DISPLAY.textContent = `${numeroUno} ${operatoreScelto}`;
+          break;
+      }
     }
-  }
   });
 }
 
 UGUALE.addEventListener("click", () => {
   switch (operatoreScelto) {
     case "+":
-      numeroDue = parseInt(DISPLAY.textContent);
+      numeroDue = parseInt(DISPLAY_DUE.textContent);
       risultato = numeroUno + numeroDue;
-      DISPLAY.textContent = risultato;
-      DISPLAY_DUE.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`
-      operatoreScelto = undefined;
-      console.log(operatoreScelto)
+      DISPLAY_DUE.textContent = risultato;
+      DISPLAY.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`;
       risultatoCalcolato = true;
+      numeroUno = undefined;
+      numeroDue = undefined;
+      risultato = undefined;
+      operatoreScelto = undefined;
       break;
 
     case "-":
-      numeroDue = parseInt(DISPLAY.textContent);
+      numeroDue = parseInt(DISPLAY_DUE.textContent);
       risultato = numeroUno - numeroDue;
-      DISPLAY.textContent = risultato;
-      DISPLAY_DUE.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`
-      operatoreScelto = undefined;
-      console.log(operatoreScelto)
+      DISPLAY_DUE.textContent = risultato;
+      DISPLAY.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`;
       risultatoCalcolato = true;
+      numeroUno = undefined;
+      numeroDue = undefined;
+      risultato = undefined;
+      operatoreScelto = undefined;
       break;
 
     case "/":
-      numeroDue = parseInt(DISPLAY.textContent);
+      numeroDue = parseInt(DISPLAY_DUE.textContent);
       risultato = numeroUno / numeroDue;
-      DISPLAY.textContent = risultato;
-      DISPLAY_DUE.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`
-      operatoreScelto = undefined;
-      console.log(operatoreScelto)
+      DISPLAY_DUE.textContent = risultato;
+      DISPLAY.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`;
       risultatoCalcolato = true;
+      numeroUno = undefined;
+      numeroDue = undefined;
+      risultato = undefined;
+      operatoreScelto = undefined;
       break;
 
     case "*":
-      numeroDue = parseInt(DISPLAY.textContent);
+      numeroDue = parseInt(DISPLAY_DUE.textContent);
       risultato = numeroUno * numeroDue;
-      DISPLAY.textContent = risultato;
-      DISPLAY_DUE.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`
-      operatoreScelto = undefined;
-      console.log(operatoreScelto)
+      DISPLAY_DUE.textContent = risultato;
+      DISPLAY.textContent = `${numeroUno} ${operatoreScelto} ${numeroDue} ${UGUALE.value}`;
       risultatoCalcolato = true;
+      numeroUno = undefined;
+      numeroDue = undefined;
+      risultato = undefined;
+      operatoreScelto = undefined;
       break;
   }
 });
 
 AC.addEventListener("click", () => {
-  risultatoCalcolato = false
+  risultatoCalcolato = false;
+  numeroUno = undefined;
+  numeroDue = undefined;
+  risultato = undefined;
+  operatoreScelto = undefined;
+  DISPLAY_DUE.textContent = "";
+});
+
+AC.addEventListener("dblclick", () => {
   DISPLAY.textContent = "";
 });
 
-AC.addEventListener('dblclick', () => {
-  risultatoCalcolato = false
-  DISPLAY_DUE.textContent = "";
-})
-
-CANCELLA.addEventListener('click', () => {
-  let arrString = DISPLAY.textContent.split('')
-  arrString.pop()
-  DISPLAY.textContent = arrString.join('')
-})
+CANCELLA.addEventListener("click", () => {
+  let arrString = DISPLAY_DUE.textContent.split("");
+  arrString.pop();
+  DISPLAY_DUE.textContent = arrString.join("");
+});
